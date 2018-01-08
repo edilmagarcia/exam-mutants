c7e9bfb6e69409efcf065b477c1787577d3a0fb1
rahnzo <alvarez.carlosalb@gmail.com>
2018-01-08 07:34:46 -0300

    Correction of stats bug in ratio calculation
    Correction of stats bug in ratio calculation

--- src/main/java/com/mercadolibre/exam/mutants/model/Stats.java
+++ src/main/java/com/mercadolibre/exam/mutants/model/Stats.java
@@ -1,27 +1,30 @@
 package com.mercadolibre.exam.mutants.model;
 
+import org.decimal4j.util.DoubleRounder;
+
 public class Stats {
 	
-	private int count_mutant_dna;
-	private int count_human_dna;
+	private double count_mutant_dna;
+	private double count_human_dna;
 	private double ratio;
 	
-	public Stats(int mutantCount, int humanCount) {
+	public Stats(double mutantCount, double humanCount) {
 		count_mutant_dna = mutantCount;
 		count_human_dna = humanCount;
 		ratio = (count_human_dna != 0) ? count_mutant_dna / count_human_dna : 0;
+		ratio = DoubleRounder.round(ratio, 2);	
 	}
 	
-	public int getCount_mutant_dna() {
+	public double getCount_mutant_dna() {
 		return count_mutant_dna;
 	}
-	public void setCount_mutant_dna(int count_mutant_dna) {
+	public void setCount_mutant_dna(double count_mutant_dna) {
 		this.count_mutant_dna = count_mutant_dna;
 	}
-	public int getCount_human_dna() {
+	public double getCount_human_dna() {
 		return count_human_dna;
 	}
-	public void setCount_human_dna(int count_human_dna) {
+	public void setCount_human_dna(double count_human_dna) {
 		this.count_human_dna = count_human_dna;
 	}
 	public double getRatio() {
